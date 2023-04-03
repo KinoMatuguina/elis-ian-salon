00ac51c4051223681f4b3992dd54582b39c6105b
Kino Matuguina <kino.matuguina@merkle.com>
2023-04-03 14:01:16 +0800

    fix config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: '/wallisconsultancy',
-  assetPrefix: '/elis-ian-salon/'
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
